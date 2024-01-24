41e82b0b1499d45f2502dd6c1198b1130cd8f61e
Hemanth-Gandupalli <155810254+Hemanth-Gandupalli@users.noreply.github.com>
2024-01-24 11:15:56 +0530

    refresh after 10 min

--- gateway/gateway.py
+++ gateway/gateway.py
@@ -33,51 +33,56 @@ LoRa.setCrcEnable(True)
 print("Set syncronize word to 0x34")
 LoRa.setSyncWord(0x34)
 print("\n-- LoRa Gateway --\n")
+recv_slot=[[1,10],[11,21],[22,32],[33,43],[44,54],[55,59]]
+sleep = [[10,11],[21,22],[32,33],[43,44],[54,55],[59,0]]
 if __name__ == "__main__":
     while True:
-        print("-------------Gateway shifts to reciving mode---------------")
-        LoRa.setRxGain(LoRa.RX_GAIN_POWER_SAVING, LoRa.RX_GAIN_AUTO)
-        LoRa.request()
-        LoRa.wait(5)
-        try:
-            rcv_data=[]
-            while LoRa.available():
-                rcv_data.append(LoRa.read())
-            print(rcv_data)
-            rcv_data = bytes(rcv_data)
-            unstruct_data = struct.unpack('7f',rcv_data)
-            # print("Recieved data--->",unstruct_data)
-            sensor1 = unstruct_data[0]
-            sensor2 = unstruct_data[1]
-            sensor3 = unstruct_data[2]
-            sensor4 = unstruct_data[3]
-            x = unstruct_data[4]
-            y = unstruct_data[5]
-            z = unstruct_data[6]
-            with open("data.json",'r') as f:
-                data = json.load(f)
-            data["sensor1"] = sensor1
-            data["sensor2"] = sensor2
-            data["sensor3"] = sensor3
-            data["sensor4"] = sensor4
-            data["x"] = x
-            data["y"] = y
-            data["z"] = z
-            with open("data.json",'w') as f:
-                json.dump(data,f)
-            with open ('status.txt','r') as file:
-                s = file.read()
-            if s == "True":
-                print("Recieved data--->",unstruct_data)
-            else:
-                print("Recieved data---> 0,0,0,0,0,0,0")
-                
-        except Exception as e:
-            print(e)
-        status = LoRa.status()
-        if status == LoRa.STATUS_CRC_ERR : print("CRC error")
-        elif status == LoRa.STATUS_HEADER_ERR : print("Packet header error")
-        time.sleep(5)
+        for i, slot in enumerate(recv_slot):
+                current_min = datetime.now().minute
+                if slot[0] <= current_min < slot[1]:
+                    print("-------------Gateway shifts to reciving mode---------------")
+                    LoRa.setRxGain(LoRa.RX_GAIN_POWER_SAVING, LoRa.RX_GAIN_AUTO)
+                    LoRa.request()
+                    LoRa.wait(4)
+                    try:
+                        rcv_data=[]
+                        while LoRa.available():
+                            rcv_data.append(LoRa.read())
+                        print(rcv_data)
+                        rcv_data = bytes(rcv_data)
+                        unstruct_data = struct.unpack('7f',rcv_data)
+                        # print("Recieved data--->",unstruct_data)
+                        sensor1 = unstruct_data[0]
+                        sensor2 = unstruct_data[1]
+                        sensor3 = unstruct_data[2]
+                        sensor4 = unstruct_data[3]
+                        x = unstruct_data[4]
+                        y = unstruct_data[5]
+                        z = unstruct_data[6]
+                        with open("data.json",'r') as f:
+                            data = json.load(f)
+                        data["sensor1"] = sensor1
+                        data["sensor2"] = sensor2
+                        data["sensor3"] = sensor3
+                        data["sensor4"] = sensor4
+                        data["x"] = x
+                        data["y"] = y
+                        data["z"] = z
+                        with open("data.json",'w') as f:
+                            json.dump(data,f)
+                        with open ('status.txt','r') as file:
+                            s = file.read()
+                        if s == "True":
+                            print("Recieved data--->",unstruct_data)
+                        else:
+                            print("Recieved data---> 0,0,0,0,0,0,0")
+                            
+                    except Exception as e:
+                        print(e)
+                    status = LoRa.status()
+                    if status == LoRa.STATUS_CRC_ERR : print("CRC error")
+                    elif status == LoRa.STATUS_HEADER_ERR : print("Packet header error")
+                    time.sleep(4)
             
 
         
